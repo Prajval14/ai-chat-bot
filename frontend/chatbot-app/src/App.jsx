c3e83e9a49ec51898b9dddf1732d445f626c659b
Prajval Patel <prajval1420@gmail.com>
2025-05-21 18:36:51 -0400

    logging env variables to se if its working

--- frontend/chatbot-app/src/App.jsx
+++ frontend/chatbot-app/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Chatbot from "./Chatbot";
 
-// Get values directly from the environment
-const BOT_NAME = import.meta.env.VITE_BOT_NAME;
-const BOT_ICON = import.meta.env.VITE_BOT_ICON;
-const BOT_COLOR = import.meta.env.VITE_BOT_COLOR;
+console.log('VITE_BOT_NAME:', import.meta.env.VITE_BOT_NAME);
+console.log('VITE_BOT_COLOR:', import.meta.env.VITE_BOT_COLOR);
+console.log('VITE_BOT_ICON:', import.meta.env.VITE_BOT_ICON);
+console.log('ALL ENVS:', import.meta.env);
+
 
 function App() {
+  const BOT_NAME = import.meta.env.VITE_BOT_NAME;
+  const BOT_ICON = import.meta.env.VITE_BOT_ICON;
+  const BOT_COLOR = import.meta.env.VITE_BOT_COLOR;
   return (
     <div>
       <Chatbot botName={BOT_NAME} botIcon={BOT_ICON} botColor={BOT_COLOR} />
