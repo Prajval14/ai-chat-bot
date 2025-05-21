4ba47cfd47938a8df0e61593f3983ad371157fe3
Prajval Patel <prajval1420@gmail.com>
2025-05-21 18:57:11 -0400

    static bot

--- frontend/chatbot-app/src/App.jsx
+++ frontend/chatbot-app/src/App.jsx
@@ -1,19 +1,14 @@
 import React from "react";
 import Chatbot from "./Chatbot";
 
-console.log('VITE_BOT_NAME:', import.meta.env.VITE_BOT_NAME);
-console.log('VITE_BOT_COLOR:', import.meta.env.VITE_BOT_COLOR);
-console.log('VITE_BOT_ICON:', import.meta.env.VITE_BOT_ICON);
-console.log('ALL ENVS:', import.meta.env);
-
+const BOT_NAME = import.meta.env.VITE_BOT_NAME;
+const BOT_ICON = import.meta.env.VITE_BOT_ICON;
+const BOT_COLOR = import.meta.env.VITE_BOT_COLOR;
 
 function App() {
-  const BOT_NAME = import.meta.env.VITE_BOT_NAME;
-  const BOT_ICON = import.meta.env.VITE_BOT_ICON;
-  const BOT_COLOR = import.meta.env.VITE_BOT_COLOR;
   return (
     <div>
-      <Chatbot botName={BOT_NAME} botIcon={BOT_ICON} botColor={BOT_COLOR} />
+      <Chatbot botName={"NestleBot"} botIcon={"https://www.shutterstock.com/image-vector/chat-bot-icon-virtual-smart-600nw-2478937553.jpg"} botColor={"#35775B"} />
     </div>
   );
 }
